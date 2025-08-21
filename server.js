01b2610721540c66f091e93b9091a1576dfa6662
NKbharath <bharathnk2004@gmail.com>
2025-08-22 03:47:20 +0530

    server change

--- server.js
+++ server.js
@@ -19,10 +19,26 @@ const { seedDatabase } = require("./seeddata");
 //     console.log("Hashed Password:", hash);
 // });
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://msma-frontend.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
 }));
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use("/api/seed", seedDatabase);
 app.use("/api/admin", adminRoutes);
