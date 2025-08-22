93cfb78016bfcf04a47ae91968465a06b24f66e8
NKbharath <bharathnk2004@gmail.com>
2025-08-22 09:34:02 +0530

    revoking

--- server.js
+++ server.js
@@ -6,27 +6,37 @@ const app = express();
 const db = require("./config/db");
 require("./models/initDB");
 const { seedDatabase } = require("./seeddata");
+// const bcrypt = require('bcrypt');
+
+// const password = 'basealpha'; 
+// const saltRounds = 10;
+
+// bcrypt.hash(password, saltRounds, (err, hash) => {
+//     if(err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log("Hashed Password:", hash);
+// });
 
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : [
-      "http://localhost:5173", 
-      "https://msma-frontend.vercel.app" 
-    ];
+app.use(cors({
+  origin: ['http://localhost:5173'], 
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use(cors())
+
 app.use(express.json());
-
 app.use("/api/seed", seedDatabase);
 app.use("/api/admin", adminRoutes);
 app.use("/api/base-commander", baseCommanderRoutes);
 
-db.serialize(() => {
-  console.log("SQLite Database ready to use");
-});
-
+db.serialize(()=>{
+    console.log("SQL lite Database ready to use");
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
